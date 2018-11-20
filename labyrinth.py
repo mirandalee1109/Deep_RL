cfce06c05aa423ec8a61c8e50cf9c6164ea473ce
Daniel Conde <danimedex@gmail.com>
2018-11-20 14:08:49 +0100

    Uptated drawing and minotaur path

--- labyrinth.py
+++ labyrinth.py
@@ -5,64 +5,29 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.table import Table
 from matplotlib import collections  as mc
-
-rewards = {}
+import random
 
 SIZE_X=6
 SIZE_Y=5
 
-#Move:
-#1:up
-#2:right
-#3:down
-#4:left
-def valid_movement(position,move):
+def min_move(position):
 
-    if move == 1:
-        if position[1] == 1:
-            return False
-        elif position[1] == 5 and (position[0] == 2 or position[0] == 3 or position[0] == 4 or position[0] == 5):
-            return False
-        elif position[1] == 3 and (position[0] == 5 or position[0] == 6):
-            return False
-        else:
-            return True
-    elif move == 2:
-        if position[0] == 6:
-            return False
-        elif position[0] == 2 and (position[1] == 1 or position[1] == 2 or position[1] == 3):
-            return False
-        elif position[0] == 4 and (position[1] == 2 or position[1] == 3 or position[1] == 5):
-            return False
-        else:
-            return True
-    elif move == 3:
-        if position[1] == 5:
-            return False
-        elif position[1] == 4 and (position[0] == 2 or position[0] == 3 or position[0] == 4 or position[0] == 5):
-            return False
-        elif position[1] == 2 and (position[0] == 5 or position[0] == 6):
-            return False
-        else:
-            return True
-    else:
-        if position[0] == 1:
-            return False
-        elif position[0] == 3 and (position[1] == 1 or position[1] == 2 or position[1] == 3):
-            return False
-        elif position[0] == 5 and (position[1] == 2 or position[1] == 3 or position[1] == 5):
-            return False
-        else:
-            return True
+    moves = []
+    if position[0] > 0:
+        moves.append(np.array([-1, 0]))
+
+    if position[0] < SIZE_X -1:
+        moves.append(np.array([1, 0]))
+
+    if position[1] > 0:
+        moves.append(np.array([0, -1]))
+
+    if position[1] < SIZE_Y -1:
+        moves.append(np.array([0, 1]))
+
+    return position + random.choice(moves)
 
 
-def average_reward(position):
-    try:
-        value = rewards[str(position)]
-    except KeyError:
-        return "Not found, calculating"
-    else:
-        return value
 
 
 def draw_image(image, player_path, min_path):
@@ -80,9 +45,17 @@ def draw_image(image, player_path, min_path):
         idx = [j % 2, (j + 1) % 2][i % 2]
         color = 'white'
 
-        tb.add_cell(i, j, width, height, text=val,
-                    loc='center', edgecolor='#63b1f2', facecolor=color)
+        if i==4 and j==4:
+            tb.add_cell(i, j, width, height,
+                        loc='center', edgecolor='#63b1f2', facecolor='#9bb4db')
 
+        elif i==0 and j==0:
+            tb.add_cell(i, j, width, height,
+                        loc='center', edgecolor='#63b1f2', facecolor='#9fe592')
+
+        else:
+            tb.add_cell(i, j, width, height,
+                        loc='center', edgecolor='#63b1f2', facecolor=color)
     # Row Labels...
     for i, label in enumerate(range(SIZE_Y)):
         tb.add_cell(i, -1, width, height, text=label+1, loc='right',
@@ -107,22 +80,22 @@ def draw_image(image, player_path, min_path):
 
     #Paths
     #May need improvements
-    last = ((player_path[0][0] - 0.5)/6,(5.5-player_path[0][1])/5)
+    last = ((player_path[0][0] + 0.5)/6,(4.5-player_path[0][1])/5)
     path=[]
     for k in range(len(player_path)):
-        path.append([last,((player_path[k][0] - 0.5)/6,(5.5-player_path[k][1])/5)])
-        last = ((player_path[k][0] - 0.5)/6,(5.5-player_path[k][1])/5)
+        path.append([last,((player_path[k][0] + 0.5)/6,(4.5-player_path[k][1])/5)])
+        last = ((player_path[k][0] + 0.5)/6,(4.5-player_path[k][1])/5)
 
     lc = mc.LineCollection(path, colors='r', linewidths=2)
 
     ax.add_collection(lc)
 
 
-    last = ((min_path[0][0] - 0.5)/6,(5.5-min_path[0][1])/5)
+    last = ((min_path[0][0] + 0.35)/6,(4.65-min_path[0][1])/5)
     path=[]
     for k in range(len(min_path)):
-        path.append([last,((min_path[k][0] - 0.5)/6,(5.5-min_path[k][1])/5)])
-        last = ((min_path[k][0] - 0.5)/6,(5.5-min_path[k][1])/5)
+        path.append([last,((min_path[k][0] + 0.35)/6,(4.65-min_path[k][1])/5)])
+        last = ((min_path[k][0] + 0.35)/6,(4.65-min_path[k][1])/5)
 
     lc = mc.LineCollection(path, colors='b', linewidths=2)
 
@@ -130,17 +103,22 @@ def draw_image(image, player_path, min_path):
 
 if __name__ == '__main__':
 
-    position=[0,0,0,0]
-    #rewards[str(position)]=1
-
-    #print(average_reward(position))
-
     values = np.zeros((SIZE_Y, SIZE_X))
 
-    player_path = [[1,1],[1,2],[1,3],[1,4],[2,4],[3,4],[4,4],[5,4],[6,4],[6,5],[5,5]]
-    #min_path = [[5,5],[5,4],[4,4],[4,3],[5,3],[5,2],[4,2],[4,3],[3,3],[2,3],[2,2]]
+    player_path = [[0,0],[0,1],[0,2],[0,3],[1,3],[2,3],[3,3],[4,3],[5,3],[5,4],[4,4]]
 
-    min_path=[[5,5]]
+    min_path=[[4,4]]
+
+    last = [4,4]
+
+    #Later len() will be T=15 or whatever
+    for i in range(len(player_path)):
+        last = min_move(last)
+        min_path.append(last)
+
+
+
+
     draw_image(values, player_path, min_path)
 
     plt.savefig('./figure_4_1.png')
