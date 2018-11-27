9a888b70a65e18e953fb5a342cb7534a8a0c0cdd
Daniel Conde <danimedex@gmail.com>
2018-11-27 13:21:11 +0100

    Qlearning

--- DP_Qlearning/ex3/qlearning.py
+++ DP_Qlearning/ex3/qlearning.py
@@ -31,10 +31,8 @@ ACTIONS = [ACTION_UP, ACTION_DOWN, ACTION_LEFT, ACTION_RIGHT, ACTION_STAY]
 
 # initial state action pair values
 START = [0, 0]
-GOAL = [1, 1]
+BANK = [1, 1]
 POLICE_START = [3, 3]
-
-police_position = POLICE_START
 
 def move_police(police_position):
     moves = []
@@ -71,7 +69,7 @@ def step(state, action, police_position):
     police_position = move_police(police_position)
 
     reward = 0
-    if next_state[0] == GOAL[0] and next_state[1] == GOAL[1]:
+    if next_state[0] == BANK[0] and next_state[1] == BANK[1]:
         reward = 1
 
     if next_state[0] == police_position[0] and next_state[1] == police_position[1]:
@@ -83,120 +81,126 @@ def step(state, action, police_position):
 
 
 # choose an action based on epsilon greedy algorithm
-def choose_action(state, q_value):
-    if np.random.binomial(1, EPSILON) == 1:
+def choose_action(state,police_position, q_value, epsi=EPSILON):
+    if np.random.binomial(1, epsi) == 1:
         return np.random.choice(ACTIONS)
     else:
-        values_ = q_value[state[0], state[1], :]
+        values_ = q_value[state[0], state[1],police_position[0],police_position[1], :]
         return np.random.choice([action_ for action_, value_ in enumerate(values_) if value_ == np.max(values_)])
 
 # an episode with Sarsa
 # @q_value: values for state action pair, will be updated
 # @step_size: step size for updating
 # @return: total rewards within this episode
-def sarsa(q_value, step_size=ALPHA):
+def sarsa(q_value, steps, episode, epsi = EPSILON):
     state = START
-    action = choose_action(state, q_value)
+    police_position = POLICE_START
+
+    action = choose_action(state,police_position, q_value, epsi)
     rewards = 0.0
-    while state != GOAL:
-        next_state, reward = step(state, action)
-        next_action = choose_action(next_state, q_value)
+    for i in range(steps):
+        total_modif = steps*episode + i
+        if total_modif == 0:
+            step_size = ALPHA
+        else:
+            step_size = 1/(total_modif**(2/3))
+
+        next_state, reward, next_police_position = step(state, action,police_position)
+        next_action = choose_action(next_state,next_police_position, q_value, epsi)
         rewards += reward
 
-        target = q_value[next_state[0], next_state[1], next_action]
+        target = q_value[next_state[0], next_state[1],next_police_position[0],next_police_position[1], next_action]
 
         target *= GAMMA
-        q_value[state[0], state[1], action] += step_size * (
-                reward + target - q_value[state[0], state[1], action])
+        q_value[state[0], state[1],police_position[0],police_position[1], action] += step_size * (
+                reward + target - q_value[state[0], state[1],police_position[0],police_position[1], action])
         state = next_state
         action = next_action
+        police_position = next_police_position
     return rewards
 
 # an episode with Q-Learning
 # @q_value: values for state action pair, will be updated
 # @step_size: step size for updating
 # @return: total rewards within this episode
-def q_learning(q_value, step_size=ALPHA):
+def q_learning(q_value, steps, episode):
     state = START
     police_position = POLICE_START
     rewards = 0.0
 
-    steps = 1000
-
     for i in range(steps):
-        action = choose_action(state, q_value)
-        next_state, reward, police_position = step(state, action, police_position)
+        total_modif = steps*episode + i
+        if total_modif == 0:
+            step_size = ALPHA
+        else:
+            step_size = 1/(total_modif**(2/3))
+
+        action = choose_action(state,police_position, q_value)
+        next_state, reward, next_police_position = step(state, action, police_position)
         rewards += reward
         # Q-Learning update
-        q_value[state[0], state[1], action] += step_size * (
-                reward + GAMMA * np.max(q_value[next_state[0], next_state[1], :]) -
-                q_value[state[0], state[1], action])
+        q_value[state[0], state[1],police_position[0],police_position[1], action] += step_size * (
+                reward + GAMMA * np.max(q_value[next_state[0], next_state[1],next_police_position[0],next_police_position[1], :]) -
+                q_value[state[0], state[1],police_position[0],police_position[1], action])
         state = next_state
+        police_position = next_police_position
     return rewards
-
-# print optimal policy
-def print_optimal_policy(q_value):
-    optimal_policy = []
-    for i in range(0, WORLD_HEIGHT):
-        optimal_policy.append([])
-        for j in range(0, WORLD_WIDTH):
-            if [i, j] == GOAL:
-                optimal_policy[-1].append('G')
-                continue
-            bestAction = np.argmax(q_value[i, j, :])
-            if bestAction == ACTION_UP:
-                optimal_policy[-1].append('U')
-            elif bestAction == ACTION_DOWN:
-                optimal_policy[-1].append('D')
-            elif bestAction == ACTION_LEFT:
-                optimal_policy[-1].append('L')
-            elif bestAction == ACTION_RIGHT:
-                optimal_policy[-1].append('R')
-    for row in optimal_policy:
-        print(row)
 
 # Use multiple runs instead of a single run and a sliding window
 # With a single run I failed to present a smooth curve
 # However the optimal policy converges well with a single run
 # Sarsa converges to the safe path, while Q-Learning converges to the optimal path
 def figure_6_4():
-    # episodes of each run
-    episodes = 500
+    # episodes
+    episodes = 1000
+    steps = 10000
 
-    # perform 40 independent runs
-    runs = 50
+    '''
+    rewards_sarsa = []
+    rewards_q_learning = []
+    episod = []
+    q_sarsa = np.zeros((WORLD_HEIGHT, WORLD_WIDTH,WORLD_HEIGHT, WORLD_WIDTH, 5))
+    q_q_learning = np.zeros((WORLD_HEIGHT, WORLD_WIDTH,WORLD_HEIGHT, WORLD_WIDTH, 5))
 
-    #rewards_sarsa = np.zeros(episodes)
-    rewards_q_learning = np.zeros(episodes)
-    for r in range(runs):
-        print("Run: " + str(r))
-        #q_sarsa = np.zeros((WORLD_HEIGHT, WORLD_WIDTH, 5))
-        q_q_learning = np.zeros((WORLD_HEIGHT, WORLD_WIDTH, 5))
-        for i in range(0, episodes):
-            #rewards_sarsa[i] += sarsa(q_sarsa)
-            rewards_q_learning[i] += q_learning(q_q_learning)
-
-    # averaging over independt runs
-    #rewards_sarsa /= runs
-    rewards_q_learning /= runs
+    for i in range(0, episodes):
+        print("Episode: " + str(i))
+        rewards_sarsa.append(sarsa(q_sarsa, steps, i))
+        rewards_q_learning.append(q_learning(q_q_learning, steps, i))
+        episod.append(i)
 
     # draw reward curves
-    #plt.plot(rewards_sarsa, label='Sarsa')
-    plt.plot(rewards_q_learning, label='Q-Learning')
+    plt.plot(episod,rewards_sarsa, label='Sarsa')
+    plt.plot(episod,rewards_q_learning, label='Q-Learning')
     plt.xlabel('Episodes')
     plt.ylabel('Sum of rewards during episode')
-    plt.ylim([-100, 0])
     plt.legend()
 
     plt.savefig('./qlearning.png')
     plt.close()
+    '''
 
-    # display optimal policy
-    #print('Sarsa Optimal Policy:')
-    #print_optimal_policy(q_sarsa)
-    print('Q-Learning Optimal Policy:')
-    print_optimal_policy(q_q_learning)
+    epsi = []
+    mean_rew = []
 
+    for j in range(1,20):
+        print(j)
+        q_sarsa = np.zeros((WORLD_HEIGHT, WORLD_WIDTH,WORLD_HEIGHT, WORLD_WIDTH, 5))
+        rewards_sarsa = []
+        epsilon = j/20
+        for i in range(0, episodes):
+            print("Episode: " + str(i))
+            rewards_sarsa.append(sarsa(q_sarsa, steps, i, epsi))
+
+        epsi.append(epsilon)
+        mean_rew.append(sum(rewards_sarsa) / float(len(rewards_sarsa)))
+
+    plt.plot(epsi,mean_rew)
+    plt.xlabel('Epsilon')
+    plt.ylabel('Mean reward')
+    plt.legend()
+
+    plt.savefig('./sarsa_epsilon.png')
+    plt.close()
 
 
 if __name__ == '__main__':
